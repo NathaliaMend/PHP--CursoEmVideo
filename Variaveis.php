f4b3c6e1e44d84d892c5603e4030587af2edc2dc
NathaliaMend <nathaliamend017@gmail.com>
2023-07-13 14:18:08 -0300

    Comecando curso de PHP

---
+++ Variaveis.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Primeiro Arquivo PHP</title>
+    <style>
+        body {
+            color: blue;
+            text-shadow: 2px 2px 2px black;
+            font-size: 30px;
+        }
+    </style>
+</head>
+<body>
+    <?php 
+    $nome = "Nathalia";
+    $idade = "22";
+    echo "A $nome tem $idade anos!"
+    ?>
+</body>
+</html>
